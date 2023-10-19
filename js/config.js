84154ec437b04958c496001a8c2008e42b6d1c8e
paul <paul7markets@hotmail.com>
2023-10-19 18:02:44 +0100

    api url

--- js/config.js
+++ js/config.js
@@ -5,6 +5,6 @@ window._config = {
         region: 'eu-west-2' // e.g. us-east-2 (eu-west-2 is London)
     },
     api: {
-        invokeUrl: '' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
+        invokeUrl: 'https://dvdqndl9z0.execute-api.eu-west-2.amazonaws.com/prod' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
     }
 };
