3b6c2bc858e4f6b02483177a05b76a27485f3420
paul <paul7markets@hotmail.com>
2023-10-19 13:08:33 +0100

    cognito

--- js/config.js
+++ js/config.js
@@ -1,8 +1,8 @@
 window._config = {
     cognito: {
-        userPoolId: '', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
-        region: '' // e.g. us-east-2 (eu-west-2 is London)
+        userPoolId: 'eu-west-2_TRCg1JDOs', // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: '60kaa0tljpjs4t6hau6bu342ki', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        region: 'eu-west-2' // e.g. us-east-2 (eu-west-2 is London)
     },
     api: {
         invokeUrl: '' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
